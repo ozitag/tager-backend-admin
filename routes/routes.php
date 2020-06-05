5560af11e76954559282e459935183a141e7b2ad
Vital Ozierski <v.ozierski@ozitag.com>
2020-06-05 14:08:45 +0300

    fix routes path

--- routes/routes.php
+++ routes/routes.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'middleware' => ['passport:administrators', 'auth:api']], function () {
-    Route::get('/self', 'Admin\Core\SelfController@index');
-    Route::post('/self/logout', 'Admin\Core\SelfController@logout');
+    Route::get('/self', OZiTAG\Tager\Backend\Admin\Controllers\SelfController::class . '@index');
+    Route::post('/self/logout', OZiTAG\Tager\Backend\Admin\Controllers\SelfController::class . '@logout');
 });
