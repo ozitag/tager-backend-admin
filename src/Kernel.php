0714fe7b90be139f98a64f0ba52e5cb3cac99e3c
Vital Ozierski <v.ozierski@ozitag.com>
2020-07-23 18:16:10 +0300

    add middleware and kernel

---
+++ src/Kernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OZiTAG\Tager\Backend\Admin;
+
+use OZiTAG\Tager\Backend\Core\Kernel\HttpKernel as HttpKernel;
+use OZiTAG\Tager\Backend\HttpCache\Middleware\CacheHttp;
+use OZiTAG\Tager\Backend\HttpCache\Middleware\DoNotCacheHttp;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'api.cache' => CacheHttp::class,
+        'api.disable-cache' => DoNotCacheHttp::class,
+        'passport' => \App\Http\Middleware\Passport::class,
+    ];
+}
