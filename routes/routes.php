0376d4d7224b4b9d914dab320b0b4b91385f2c2d
Vital Ozierski <vital@ozerich.com>
2022-11-30 15:38:30 +0300

    1.6.3 - upload endpoint use tager core feature

--- routes/routes.php
+++ routes/routes.php
@@ -4,14 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 use OZiTAG\Tager\Backend\Auth\Http\Controllers\AuthLogController;
 use OZiTAG\Tager\Backend\Admin\Controllers\SelfController;
-use OZiTAG\Tager\Backend\Files\TagerBackendFilesController;
+use OZiTAG\Tager\Backend\Admin\Controllers\UploadFileController;
 
 Route::group(['prefix' => 'admin', 'middleware' => ['passport:administrators', 'auth:api']], function () {
-    Route::post('/upload', [TagerBackendFilesController::class, 'upload']);
-    Route::get('/auth-logs/{provider_string}', [AuthLogController::class, 'index']);
-
+    Route::post('/upload', [UploadFileController::class, 'upload']);
     Route::get('/self', [SelfController::class, 'getProfile']);
     Route::post('/self', [SelfController::class, 'updateProfile']);
     Route::post('/self/password', [SelfController::class, 'changePassword']);
     Route::post('/self/logout', [SelfController::class, 'logout']);
+
+
+    Route::get('/auth-logs/{provider_string}', [AuthLogController::class, 'index']);
 });
---
+++ src/Controllers/UploadFileController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OZiTAG\Tager\Backend\Admin\Controllers;
+
+use OZiTAG\Tager\Backend\Core\Controllers\Controller;
+use OZiTAG\Tager\Backend\Core\Features\UploadFileFeature;
+
+class UploadFileController extends Controller
+{
+    public function upload()
+    {
+        return $this->serve(UploadFileFeature::class);
+    }
+}
